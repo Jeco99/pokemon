67e3c6b675d35c7fe2a9e28758e42825051a665d
Jeco99 <jerriccojohncabunagan00@gmail.com>
2024-04-13 08:10:05 +0800

    feat: search for the name and type

--- component/Search/search.tsx
+++ component/Search/search.tsx
@@ -1,7 +1,25 @@
-export default function Search() {
+import { SearchDataProp } from "@/utils/pokemonDataType";
+import React, { ChangeEvent, useState } from "react";
+import { TextInput } from "@mantine/core";
+
+const SearchBar: React.FC<SearchDataProp> = ({ onSearch }) => {
+  const [search, setSearch] = useState<string>("");
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const searchQuery = event.target.value;
+    setSearch(searchQuery);
+    onSearch(searchQuery);
+  };
   return (
     <>
       <h1>Search</h1>
+      <TextInput
+        label="Search Pokemon"
+        value={search}
+        onChange={handleChange}
+      />
     </>
   );
-}
+};
+
+export default SearchBar;
